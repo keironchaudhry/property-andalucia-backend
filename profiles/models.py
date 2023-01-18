c5de48847cfde892c56b022b372f83d0812fec0b
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-18 11:59:28 +0000

    Create profile model and admin

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+
+    owner = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    name = models.CharField(
+        max_length=255,
+        blank=True
+    )
+    bio = models.TextField(
+        blank=True
+    )
+    telephone = models.CharField(
+        max_length=20,
+        blank=True
+    )
+    image = models.ImageField(
+        upload_to='images/',
+        default='../default_profile_hr9s8z'
+    )
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateField(
+        auto_now=True
+    )
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"{self.owner}'s profile"
