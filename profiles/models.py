b23d96e82cdee2ff36f426552ac6cd47c25bed8c
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-18 12:53:45 +0000

    Replace User relation in profile model with new updated user

--- profiles/models.py
+++ profiles/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Profile(models.Model):
 
     owner = models.OneToOneField(
-        User,
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE
     )
     name = models.CharField(
