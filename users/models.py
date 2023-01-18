5a60585ad774988f9c636d6adc9988a5d168601d
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-18 12:51:12 +0000

    Create updated user model using abstractuser

--- users/models.py
+++ users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class UpdatedUser(AbstractUser):
+    seller_status = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return self.username
+
+    def get_seller_status(self):
+        return self.seller_status
