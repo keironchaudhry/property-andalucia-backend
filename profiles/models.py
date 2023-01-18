d3d8192147d58509524ba1c1f8d27c49d519aafc
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-18 21:53:51 +0000

    Create signals for profile creation

--- profiles/models.py
+++ profiles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.conf import settings
+from django.db.models.signals import post_save
 
 
 class Profile(models.Model):
@@ -39,3 +40,16 @@ class Profile(models.Model):
 
     def __str__(self):
         return f"{self.owner}'s profile"
+
+
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(
+            owner=instance
+        )
+
+
+post_save.connect(
+    create_profile,
+    sender=settings.AUTH_USER_MODEL
+)
