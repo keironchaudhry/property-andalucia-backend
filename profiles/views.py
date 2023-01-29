ad2caab15ea9e65cd25279cd65d8df0364a9397e
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-01-29 19:54:40 +0000

    Create permissions.py for object permissions

--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,17 @@
 from rest_framework import generics
 from .models import Profile
 from .serializers import ProfileSerializer
+from property_andalucia_api.permissions import IsOwnerOrReadOnly
 
 
 class ProfileList(generics.ListAPIView):
+    """ Obtains and lists all profile objects """
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
 
 
 class ProfileDetail(generics.RetrieveUpdateAPIView):
+    """ Retrieve or update a profile when "owner" """
+    permission_classes = [IsOwnerOrReadOnly]
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
