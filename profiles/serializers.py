8805b486e17f2a7406a3c4b380643d55110f264f
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-04 08:28:33 +0000

    Retrieve following id from profile objects

--- profiles/serializers.py
+++ profiles/serializers.py
@@ -1,15 +1,28 @@
 from rest_framework import serializers
 from .models import Profile
+from followers.models import Follower
 
 
 class ProfileSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     seller_status = serializers.ReadOnlyField(source='owner.seller_status')
+    following_id = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_following_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            following = Follower.objects.filter(
+                owner=user,
+                followed=obj.owner
+            ).first()
+            return following.id if following else None
+        return None
+
 
     class Meta:
         model = Profile
@@ -18,6 +31,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             'owner',
             'is_owner',
             'seller_status',
+            'following_id',
             'name',
             'bio',
             'email',
