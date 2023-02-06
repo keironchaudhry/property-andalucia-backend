8882a12b24534ec2b3be8a3c3d30703aa5820c96
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-06 14:35:57 +0000

    Override get_queryset method to display queryset only to object owner

--- notes/views.py
+++ notes/views.py
@@ -6,12 +6,26 @@ from property_andalucia_api.permissions import IsOwnerOrReadOnly
 
 class NoteList(generics.ListCreateAPIView):
     """ Obtains and lists all note objects """
+    model = Note
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Note.objects.all()
     serializer_class = NoteSerializer
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
+
+# https://www.django-rest-framework.org/api-guide/filtering/
+    def get_queryset(self):
+        """ Reveals object queryset based on whether the defined user
+        is the owner of the object or is anonymous """
+        user = self.request.user
+        if user.is_authenticated:
+            queryset = self.model.objects.filter(
+                owner=user
+            )
+        else:
+            queryset = self.model.objects.none()
+        return queryset
 
 
 class NoteDetail(generics.RetrieveUpdateDestroyAPIView):
