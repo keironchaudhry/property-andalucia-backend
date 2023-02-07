42ed5839f6347a13aa6b5aa8f90f67f217031508
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-07 11:55:38 +0000

    Create logic to validate numbers-only value

--- property_andalucia_api/validators.py
+++ property_andalucia_api/validators.py
@@ -1,10 +1,16 @@
+from django.core.validators import RegexValidator
 from rest_framework import serializers
 
-# Credit owed to Code Institute Django REST Framework project for code basis.
-# https://github.com/Code-Institute-Solutions/drf-api
+validate_numbers = RegexValidator(
+    (
+        '^\\d+$'
+    )
+)
 
 
 def validate_image(value):
+    # Credit owed to CI Django REST Framework project for code basis.
+    # https://github.com/Code-Institute-Solutions/drf-api
     max_height = 4096
     max_width = 4096
     maximum_file_size = 2
