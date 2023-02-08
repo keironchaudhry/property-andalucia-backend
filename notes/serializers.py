62275caea82a72ab14ef6346b00b0ce79d53d8a6
Keiron Chaudhry <keironliamchaudhry@outlook.com>
2023-02-08 10:40:32 +0000

    Return naturaltime object for 'created/updated at' fields

--- notes/serializers.py
+++ notes/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.humanize.templatetags.humanize import naturaltime
 from rest_framework import serializers
 from .models import Note
 
@@ -11,6 +12,12 @@ class NoteSerializer(serializers.ModelSerializer):
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_created_at(self, obj):
+        return naturaltime(obj.created_at)
+
+    def get_updated_at(self, obj):
+        return naturaltime(obj.updated_at)
 
     class Meta:
         model = Note
